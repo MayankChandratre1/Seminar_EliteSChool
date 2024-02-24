fd606cdc57562078df4eb9e3db1fb17f2cf73583
Mayank <mayankmchandratre@gmail.com>
2024-02-24 08:19:58 +0530

    add students council

---
+++ modal.js
@@ -0,0 +1,29 @@
+let modalContainer = document.getElementById("contactModal");
+let modal = document.getElementsByClassName("modal")[0];
+
+modalContainer.addEventListener("click",(e)=>{
+    if(!(e.target.className === "modal")){
+        toggleModal();
+    }
+})
+
+document.querySelectorAll(".contact").forEach((button)=>{
+    button.addEventListener( "click" , ()=>{
+           toggleModal();
+    })
+})
+
+
+
+function toggleModal(){
+    if(modalContainer.style.display != "none"){
+
+        modalContainer.style.display = "none";
+        modalContainer.style.scale = "0";
+    }
+    else{
+        
+        modalContainer.style.display = "flex";
+        modalContainer.style.scale = "1";
+    }
+}
